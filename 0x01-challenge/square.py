2f2ae233dbf9c028adc2bf336813de1d3c487548
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-05 22:51:16 +0300

    fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" python file"""
 
 class square():
-    
+    """ square class"""
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """ init here """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,9 +17,11 @@ class square():
         return self.width * self.width
 
     def permiter_of_mySquare(self):
+        """ return permiter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ return str doc"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
